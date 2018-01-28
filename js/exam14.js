e8cf2665c89b9cc6556ca63c88b1f7aee01c4120
Johknotter Hill <johknotterhill@gmail.com>
2018-01-28 18:08:04 -0500

    Complete

--- js/exam14.js
+++ js/exam14.js
@@ -16,6 +16,7 @@ app14.controller("mainCtrl", function($scope, $window, $location, $interval, $lo
   $location.search("randon=stuff");
   $scope.currSearch = $location.search();
 
+
   $interval(function() {
     var hour = new Date().getHours();
 
@@ -27,6 +28,43 @@ app14.controller("mainCtrl", function($scope, $window, $location, $interval, $lo
   }, 2000);
 
 
+  // $scope.$log = $log;
 
+  // $scope.throwException = function() {
+  //     try {
+  //       throw new Error("Exception Thrown");
+  //     }
+  //     catch(e) {
+  //       $exceptionHandler(e.message, "Caught Exception");
+  //     }
+  // };
+
+  // $scope.badStuff = "";
+  //
+  // $scope.$watch("badStuff", function(value) {
+  //   $scope.htmlData = $sanitize(value);
+  // });
+
+
+  $scope.players = [{name: "Barry Bonds", avg: 0.298, hr: 762, obp: 0.444},
+  {name: "Hank Aaron", avg: 0.305, hr: 755, obp: 0.374},
+  {name: "Babe Ruth", avg: 0.342, hr: 714, obp: 0.474},
+  {name: "Ted Williams", avg: 0.344, hr: 521, obp: 0.482}];
+
+});
+
+
+app14.directive("getPlayerInfo", function($compile) {
+  return function(scope, element, attrs) {
+
+    var playerList = "<ul><li ng-repeat='player in players'>{{player.name}}</li></ul>";
+
+    var listElem = angular.element(playerList);
+
+    var compileFunc = $compile(listElem);
+    compileFunc(scope);
+
+    element.append(listElem);
+  };
 
 });
